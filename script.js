2e87d8e316627a0e60609ca09ba3ed8ddce80b3b
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2023-11-14 18:38:42 -0700

    created list of possible characters to choose from based on user inputs

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/* 
+REQUIREMENTS
+------------
+1. Length must be between 8-128 characters.
+2. User can choose to include lowercase, uppercase, numeric, and/or special characters.
+3. User MUST choose AT LEAST ONE character type.
+
+PSUEDO CODE
+------------
+1. Prompt for desired length.
+2. Prompt for including lowercase.
+3. Prompt for including uppercase.
+4. Prompt for including numeric.
+5. Prompt for including special characters.
+6. Input validation.
+7. Generate password.
+8. Display on page.
+*/
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,37 +29,15 @@ function writePassword() {
 }
 
 function generatePassword() {
-  /* 
-  REQUIREMENTS
-  ------------
-  1. Length must be between 8-128 characters.
-  2. User can choose to include lowercase, uppercase, numeric, and/or special characters.
-  3. User MUST choose AT LEAST ONE character type.
-
-  PSUEDO CODE
-  ------------
-  1. Prompt for desired length.
-  2. Prompt for including lowercase.
-  3. Prompt for including uppercase.
-  4. Prompt for including numeric.
-  5. Prompt for including special characters.
-  6. Regenerate prompt if Requirement #1 and/or Requirement #3 is not met.
-  7. Generate password.
-  8. Display on page.
-  */
-
   // 1. Prompt for desired length.
   var length = validateLength();
 
   // 2. Prompt for including lowercase characters.
   var includeLowerCase = window.confirm("Include lowercase?");
-
   // 3. Prompt for including uppercase characters.
   var includeUpperCase = window.confirm("Include uppercase?");
-
   // 4. Prompt for including numeric characters.
   var includeNumbers = window.confirm("Include numbers?");
-
   // 5. Prompt for including special characters.
   var includeSpecialCharacters = window.confirm("Include special characters?");
 
@@ -48,6 +45,15 @@ function generatePassword() {
   var characters = [includeLowerCase, includeUpperCase, includeNumbers, includeSpecialCharacters];
   // makes sure at least one of the 4 types is selected
   characters = validateCharacterTypes(characters);
+
+  // all possible lists of characters to choose from (at least for this project)
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseLetters = lowerCaseLetters.toUpperCase();
+  const numbers = "0123456789";
+  const specialCharacters = `!"#$%&'()*+,-./:;<=>?@[]^_{|}~`;
+
+  // creates string of characters to choose from, used to generate password
+  let characterLibrary = "";
 }
 
 // Prompts for length and redisplays if requirements aren't met.
